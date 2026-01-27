fde66f00b394777b3a93e78df61b5dc443f6ddcd
MarcusDavidG <davidmarcus827@gmail.com>
2026-01-27 21:50:03 +0100

    feat(sdk): add create task transaction

---
+++ lib/transactions/create-task.ts
@@ -0,0 +1,11 @@
+import { uintCV, stringAsciiCV } from '@stacks/transactions';
+import { buildTransaction } from '../transaction-builder';
+
+export const createTaskTransaction = (title: string, description: string, amount: number, deadline: number, priority: number, category: number) => {
+  return buildTransaction(
+    process.env.NEXT_PUBLIC_CONTRACT_ADDRESS!,
+    'bittask',
+    'create-task',
+    [stringAsciiCV(title), stringAsciiCV(description), uintCV(amount), uintCV(deadline), uintCV(priority), uintCV(category)]
+  );
+};
